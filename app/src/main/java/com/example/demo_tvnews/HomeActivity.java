6cae91d6b2fc101dc725f5e1fa6fdd404af9fbeb
cpx-xing <2625185550@qq.com>
2021-03-24 00:28:31 +0800

    第二次提交，增加了首页切换功能

--- app/src/main/java/com/example/demo_tvnews/HomeActivity.java
+++ app/src/main/java/com/example/demo_tvnews/HomeActivity.java
@@ -1,14 +1,68 @@
 package com.example.demo_tvnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.demo_tvnews.adapter.MyPagerAdapter;
+import com.example.demo_tvnews.entity.TabEntity;
+import com.example.demo_tvnews.fragment.CollectFragment;
+import com.example.demo_tvnews.fragment.HomeFragment;
+import com.example.demo_tvnews.fragment.MyFragment;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private CommonTabLayout commonTabLayout;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+
+    //    底部的标签
+    private String[] mTitles = {"首页", "收藏", "我的"};
+    //    为点击时的图案
+    private int[] mIconUnselectIds = {
+            R.mipmap.home, R.mipmap.message,
+            R.mipmap.mine};
+    //    点击时的图案
+    private int[] mIconSelectIds = {
+            R.mipmap.home2, R.mipmap.message2,
+            R.mipmap.mine2};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        viewPager = findViewById(R.id.viewpager);
+        commonTabLayout = findViewById(R.id.commonTabLayout);
+
+        mFragments.add(HomeFragment.newInstance());
+        mFragments.add(CollectFragment.newInstance());
+        mFragments.add(MyFragment.newInstance());
+
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+
+        commonTabLayout.setTabData(mTabEntities);
+
+        commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+            }
+        });
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(),mTitles,mFragments));
+
     }
 }
---
+++ app/src/main/java/com/example/demo_tvnews/entity/TabEntity.java
@@ -0,0 +1,30 @@
+package com.example.demo_tvnews.entity;
+
+import com.flyco.tablayout.listener.CustomTabEntity;
+// 点击的对象
+public class TabEntity implements CustomTabEntity {
+    public String title;
+    public int selectedIcon;
+    public int unSelectedIcon;
+
+    public TabEntity(String title, int selectedIcon, int unSelectedIcon) {
+        this.title = title;
+        this.selectedIcon = selectedIcon;
+        this.unSelectedIcon = unSelectedIcon;
+    }
+
+    @Override
+    public String getTabTitle() {
+        return title;
+    }
+
+    @Override
+    public int getTabSelectedIcon() {
+        return selectedIcon;
+    }
+
+    @Override
+    public int getTabUnselectedIcon() {
+        return unSelectedIcon;
+    }
+}
